979003223ed7250d942cfad2ebd2ed76c3f0bcab
Grace Callanta <callantag@gmail.com>
2021-09-13 16:17:12 +0800

    Create signup, login (with google login) and admin page

---
+++ src/components/Account/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { AuthUserContext, withAuthorization } from "../Session";
+import { PasswordForgetForm } from "../PasswordForget";
+import PasswordChangeForm from "../PasswordChange";
+
+const SIGN_IN_METHODS = [
+  {
+    id: "password",
+    provider: null,
+  },
+  {
+    id: "google.com",
+    provider: "googleProvider",
+  },
+];
+
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {(authUser) => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
+);
+
+const condition = (authUser) => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
